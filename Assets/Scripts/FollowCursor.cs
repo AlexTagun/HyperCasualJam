8bc416df314c45f046962c0ed494e4ad15b6fef9
AlexKutsar <alekcandr_123qw@mail.ru>
2020-06-12 19:21:06 +0300

    add hammer follow cursor

---
+++ Assets/Scripts/FollowCursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Camera _camera;
+    void Start()
+    {
+        _camera = Camera.main;
+        transform.position = _camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = _camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
