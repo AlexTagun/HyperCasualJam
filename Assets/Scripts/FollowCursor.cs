8be77ac0253b1f96af6afe0e03610a0590ec86a7
AlexKutsar <alekcandr_123qw@mail.ru>
2020-06-13 02:16:51 +0300

    redone hammer movement to cursor

--- Assets/Scripts/FollowCursor.cs
+++ Assets/Scripts/FollowCursor.cs
@@ -6,15 +6,28 @@ public class FollowCursor : MonoBehaviour
 {
     // Start is called before the first frame update
     private Camera _camera;
+    private Rigidbody2D _rigidbody2D;
+
+    [SerializeField] private float moveSpeed;
+
+    private Vector3 mousePosition;
+    private Vector2 position;
     void Start()
     {
         _camera = Camera.main;
+        _rigidbody2D = GetComponent<Rigidbody2D>();
         transform.position = _camera.ScreenToWorldPoint(Input.mousePosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = _camera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition(position);
     }
 }
