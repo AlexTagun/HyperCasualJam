4663cc6162d3a5b370aed381166a5c0cf073f656
AlexKutsar <alekcandr_123qw@mail.ru>
2020-06-13 02:21:56 +0300

    fixed  hammer`s center of mass

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class Hammer : MonoBehaviour
 {
-    [SerializeField] private GameObject centerOfMass;
-    private Rigidbody2D _rigidbody;
 
-    // Start is called before the first frame update
     void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _rigidbody.centerOfMass = centerOfMass.transform.position;
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
