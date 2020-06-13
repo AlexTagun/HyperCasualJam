52589edd3940dc296df0574355478411f831f325
AlexKutsar <alekcandr_123qw@mail.ru>
2020-06-13 18:41:47 +0300

    add conveyor movement

---
+++ Assets/Scripts/ConveyorMovement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private List<GameObject> _partsConveyor = new List<GameObject>();
+    public List<GameObject> PartsConveyor => _partsConveyor;
+    [SerializeField] private Vector2 _directionMove;
+    [SerializeField] private float _speedMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        MoveConveyor();
+    }
+
+    private void MoveConveyor()
+    {
+        _rigidbody2D.velocity = _directionMove * _speedMove;
+    }
+    public void MoveBoardToEnd()
+    {
+        var boardBehindScreen = _partsConveyor[0];
+        _partsConveyor.Remove(boardBehindScreen);
+        boardBehindScreen.transform.position = new Vector2(_partsConveyor[_partsConveyor.Count - 1].transform.position.x + boardBehindScreen.transform.localScale.x * boardBehindScreen.GetComponent<BoxCollider2D>().size.x, boardBehindScreen.transform.position.y);
+        _partsConveyor.Add(boardBehindScreen);
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -18,6 +18,7 @@ public static class EventManager
     {
         OnEndGame?.Invoke();
     }
+
     // Start is called before the first frame update
 
 }
