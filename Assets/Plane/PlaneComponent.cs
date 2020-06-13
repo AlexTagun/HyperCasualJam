57a5a0a0c892dd24ad82b42dd8b02113abdaf102
alexeytagun <alexey.tagun@game-garden.com>
2020-06-13 21:49:11 +0300

    add plane returning logic

--- Assets/Plane/PlaneComponent.cs
+++ Assets/Plane/PlaneComponent.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -11,26 +12,55 @@ public class PlaneComponent : MonoBehaviour {
     [SerializeField] private Transform directionPoint1;
     [SerializeField] private Transform directionPoint2;
     [SerializeField] private float speed;
+    [SerializeField] private float accuracy;
 
     private bool _isMouseDown = false;
     private Vector2 _mouseDelta;
+    private Vector2 _startLocalPosition;
+    private bool _isMoving;
 
     private Vector2 CurPosition => new Vector2(transform.position.x, transform.position.y);
+    private Vector2 CurLocalPosition => new Vector2(transform.localPosition.x, transform.localPosition.y);
     private Vector2 MousePosition => Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-    private void Start() {
-        Debug.Log("Push");
+    private void Awake() {
+        _startLocalPosition = CurLocalPosition;
+        
         StartCoroutine(StartPushTimer());
     }
 
+    private void Start() {
+        Debug.DrawLine(transform.position, transform.position + Vector3.left * accuracy, Color.red, 500);
+    }
+
     private void OnMouseDown() {
+        if(!_isMoving) return;
         _isMouseDown = true;
         _mouseDelta = CurPosition - MousePosition;
     }
     
     private void OnMouseUp() {
         _isMouseDown = false;
+        // Debug.Log(Mathf.Abs(_startLocalPosition - CurPosition).ma);
+        if((_startLocalPosition - CurPosition).magnitude > accuracy) return;
+        Debug.Log(_isMoving);
+        if(!_isMoving) return;
+        Debug.Log("stop");
+        
+        _isMoving = false;
+        // rigidbody.isKinematic = true;
+        // transform.localPosition = _startLocalPosition;
+        var parentTransform = GetComponentInParent<Transform>();
+        // transform.TransformPoint(_startLocalPosition)
+        Debug.Log($"parent: {parentTransform.gameObject.name}");
+        Debug.Log($"parent: {parentTransform.position}");
+        Debug.Log($"local: {_startLocalPosition}");
+        Debug.Log($"world: {parentTransform.TransformPoint(_startLocalPosition)}");
+        var pos = transform.parent.TransformPoint(_startLocalPosition);
+        rigidbody.MovePosition( pos);
+        
         rigidbody.velocity = Vector2.zero;
+        Debug.Log("start coroutine");
         StartCoroutine(StartPushTimer());
     }
 
@@ -41,15 +71,14 @@ public class PlaneComponent : MonoBehaviour {
 
     private void Push() {
         Debug.Log("Push");
+        // rigidbody.isKinematic = false;
+        _isMoving = true;
         rigidbody.AddForce(GetRandomDirection() * speed, ForceMode2D.Impulse);
     }
 
     private IEnumerator StartPushTimer() {
-        Debug.Log("Push");
         var timeToPush = Random.Range(throwRangeFrom, throwRangeTo);
         yield return new WaitForSeconds(timeToPush);
-        // yield return new WaitForSeconds(0f);
-        Debug.Log("Push");
         Push();
     }
 
