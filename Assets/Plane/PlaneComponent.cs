b0b29147a1bdec292c157f9c87c6a5781a48aa29
alexeytagun <alexey.tagun@game-garden.com>
2020-06-13 15:40:11 +0300

    add push logic

--- Assets/Plane/PlaneComponent.cs
+++ Assets/Plane/PlaneComponent.cs
@@ -2,17 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlaneComponent : MonoBehaviour {
-    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private float throwRangeFrom;
+    [SerializeField] private float throwRangeTo;
+    [SerializeField] private Vector2 direction;
+    [SerializeField] private float speed;
 
     private bool _isMouseDown = false;
     private Vector2 _mouseDelta;
 
     private Vector2 CurPosition => new Vector2(transform.position.x, transform.position.y);
     private Vector2 MousePosition => Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    
 
+    private void Start() {
+        Debug.Log("Push");
+        StartCoroutine(StartPushTimer());
+    }
 
     private void OnMouseDown() {
         _isMouseDown = true;
@@ -21,10 +29,26 @@ public class PlaneComponent : MonoBehaviour {
     
     private void OnMouseUp() {
         _isMouseDown = false;
+        rigidbody.velocity = Vector2.zero;
+        StartCoroutine(StartPushTimer());
     }
 
     private void Update() {
         if(!_isMouseDown) return;
-        _rigidbody.MovePosition(MousePosition + _mouseDelta);
+        rigidbody.MovePosition(MousePosition + _mouseDelta);
+    }
+
+    private void Push() {
+        Debug.Log("Push");
+        rigidbody.AddForce(direction * speed, ForceMode2D.Impulse);
+    }
+
+    private IEnumerator StartPushTimer() {
+        Debug.Log("Push");
+        var timeToPush = Random.Range(throwRangeFrom, throwRangeTo);
+        yield return new WaitForSeconds(timeToPush);
+        // yield return new WaitForSeconds(0f);
+        Debug.Log("Push");
+        Push();
     }
 }
