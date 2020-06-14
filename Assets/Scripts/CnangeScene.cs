0f81dcf3c818abc64103b4e8b4c163e119e6fbbd
AlexKutsar <alekcandr_123qw@mail.ru>
2020-06-14 23:07:11 +0300

    add transition between scenes

---
+++ Assets/Scripts/CnangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CnangeScene : MonoBehaviour
+{
+
+
+    public void LoadSceneHammer()
+    {
+        SceneManager.LoadScene("Hammer");
+    }
+    public void LoadSceneMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void LoadScenePlane()
+    {
+        SceneManager.LoadScene("Plane");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
