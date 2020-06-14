6f0494e892ad1b2194b8e66fb50dbce48cc2a092
vsemenyakin <semenyakinVS@gmail.com>
2020-06-14 23:39:44 +0300

    - Added timer to Hummer Game

---
+++ Assets/GameTimerObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameTimerObject : MonoBehaviour
+{
+    private void FixedUpdate() {
+        _text.text = getTimeString(Time.time);
+    }
+
+    private string getTimeString(float inTime) {
+        System.TimeSpan timeSpan = System.TimeSpan.FromSeconds(inTime);
+        return string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+   }
+
+    [SerializeField] private UnityEngine.UI.Text _text = null;
+}
