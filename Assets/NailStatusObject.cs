58f0905f1f2f9e0e1261b9fe430d00676092e6c9
vsemenyakin <semenyakinVS@gmail.com>
2020-06-14 15:13:13 +0300

    - Added status effects for marking "finished" Nails

--- Assets/NailStatusObject.cs
+++ Assets/NailStatusObject.cs
@@ -1,9 +1,11 @@
 ﻿using UnityEngine;
+using DG.Tweening;
 
 public class NailStatusObject : MonoBehaviour
 {
     public void setNailState(float inPassedHeight, float inHeightToPass) {
         setNailLeftHeight(inPassedHeight, inHeightToPass);
+        setNailFinishedStatus(inPassedHeight, inHeightToPass);
     }
 
     private void setNailLeftHeight(float inPassedHeight, float inHeightToPass) {
@@ -20,11 +22,41 @@ public class NailStatusObject : MonoBehaviour
         _nailLeftHeightMask.transform.localScale = theLocalScale;
     }
 
+    private void setNailFinishedStatus(float inPassedHeight, float inHeightToPass) {
+        if (0 != inPassedHeight && inPassedHeight >= inHeightToPass)
+            showMailFinishedStatusAnimation();
+    }
+
+    private void showMailFinishedStatusAnimation() {
+        Transform theImageTransform = _nailFinishedStatusImage.transform;
+
+        float theOriginalScale = theImageTransform.localScale.x;
+        theImageTransform.DOScale(theOriginalScale * 2f, 0f);
+        theImageTransform.DOScale(theOriginalScale * 1f, 1f);
+
+        Color theFinalColor = _nailFinishedStatusImage.color;
+        theFinalColor.a = 0.7f;
+        _nailFinishedStatusImage.DOColor(theFinalColor, 1f);
+    }
+
+    private void Awake() {
+        initNailFinishedStatusInvisibility();
+    }
+
+    private void initNailFinishedStatusInvisibility() {
+        Color theColor = _nailFinishedStatusImage.color;
+        theColor.a = 0f;
+        _nailFinishedStatusImage.color = theColor;
+    }
+
     private void FixedUpdate() {
+        updateLeftHeightMaskAlpha();
+    }
+
+    private void updateLeftHeightMaskAlpha() {
         Vector2 theMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 thePosition = transform.position;
         float theDistanceToMouse = (thePosition - theMousePosition).magnitude;
-        Debug.Log(theDistanceToMouse);
 
         float theAlphaFactor = 0f;
         if (theDistanceToMouse < _fullAlphaDistane)
@@ -39,6 +71,7 @@ public class NailStatusObject : MonoBehaviour
 
     //Fields
     [SerializeField] SpriteRenderer _nailLeftHeightMask = null;
+    [SerializeField] SpriteRenderer _nailFinishedStatusImage = null;
     [SerializeField] float _fullAlpha = 0.7f;
     [SerializeField] float _fullAlphaDistane = 3f;
     [SerializeField] float _zeroAlphaDistane = 10f;
